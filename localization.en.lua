1119f9fe57f5d687d70900595be4a68eb88b7585
Ryad M <ryad@ryme-dev.com>
2022-10-27 10:15:19 +0200

    Prepatch Dragonflight
    Wow 10 Fixes.
    Edition mode supported.  
    Options removed: MaxBuffs / Raid always shown

--- localization.en.lua
+++ localization.en.lua
@@ -8,13 +8,14 @@ KRF_VERS_TITLE    = format("%s %s", KRF_TITLE, KRF_VERSION);
 
 -- Whats new info
 KRF_WHATSNEW = OR.."- "..KRF_VERS_TITLE..YLL.." - What's new:|r\n"
-  .."- Ready for Shadowlands 9.2.7\n"
+  .."- Ready for DragonFlight prepatch 10.0.0\n"
+  .."- Removed: MaxBuffs and Always show raid\n"
   ;
 
 KRF_SUBTITLE      = "Raid frames support";
 KRF_DESC          = "Enhance party/raid frames and nameplates on friendly units\n\n"
 .." - Highlight raid frames background on low health\n\n"
-.." - Inverted frames will use your colors directly\n\n"
+.." - Inverted frames will use your choice of colors\n\n"
 .." - Transparency when unit out of range";
 KRF_OPTIONS_TITLE = format("%s - Options", KRF_VERS_TITLE);
 
@@ -26,17 +27,17 @@ KRF_INIT_FAILED = format("%s not initialized correctly!", KRF_VERS_TITLE);
 
 
 KRF_OPTION_RAID_HEADER = "Party / Raid";
-KRF_OPTION_HIGHLIGHTLOWHP = "Highlight players HP loss";
+KRF_OPTION_HIGHLIGHTLOWHP = "Highlight players HP loss (dynamic colors)";
 KRF_OPTION_REVERTBAR = YL.."Revert|r HP bars (less life = bigger bar !) "..YL.."*";
-KRF_OPTION_HEALTH_LOW = "Near dead!";
+KRF_OPTION_HEALTH_LOW = "Almost dead!";
 KRF_OPTION_HEALTH_LOW_TOOLTIP = "Low health color applied "..YLL.."BELOW|r this limit\n\n"
-  .."i.e.: Red below 30%";
+  .."i.e.: Red below 25%";
 KRF_OPTION_HEALTH_WARN = "Warning";
 KRF_OPTION_HEALTH_WARN_TOOLTIP = "Warn health color applied "..YLL.."AT|r this limit exactly\n\n"
   .."i.e.: Yellow at 50%";
 KRF_OPTION_HEALTH_OK = "Health ok";
 KRF_OPTION_HEALTH_OK_TOOLTIP = "OK health color applied "..YLL.."AFTER|r this limit\n\n"
-  .."i.e.: Green after 60%";
+  .."i.e.: Green after 75%";
 KRF_OPTION_MOVEROLEICONS = "Adjust role icons on top left";
 KRF_OPTION_HIDEDAMAGEICONS = "Hide 'dps' role icon";
 KRF_OPTION_HIDEREALM = "Hide players realm";
@@ -45,21 +46,22 @@ KRF_OPTION_NOTINRANGE = "Transparency when out of range";
 KRF_OPTION_NOTINCOMBAT = "Raid transparency out of combat";
 KRF_OPTION_SOLORAID = CY.."Always display raid frames "..YL.."*";
 KRF_OPTION_DEBUG_ON = "! Test raidframes !";
-KRF_OPTION_DEBUG_ON_MESSAGE = "Testing raid frames, reclick to stop it!";
+KRF_OPTION_DEBUG_ON_MESSAGE = "Testing raid frames (you can test in Edit Mode)\n"
+                  .."Reclick to stop it!";
 KRF_OPTION_DEBUG_OFF = "! STOP Test !";
 KRF_OPTION_DEBUG_OFF_MESSAGE = "Test stopped, have fun!";
 
 KRF_OPTION_BUFFS_HEADER = "Buffs / Debuffs";
-KRF_OPTION_BUFFSSCALE = "Buffs relative size";
-KRF_OPTION_DEBUFFSSCALE = "Debuffs relative size";
+KRF_OPTION_BUFFSSCALE = "Buffs relative size*";
+KRF_OPTION_DEBUFFSSCALE = "Debuffs relative size*";
 KRF_OPTION_MAXBUFFS = "Max buffs";
 KRF_OPTION_MAXBUFFS_TOOLTIP = "Max buffs to display";
 KRF_OPTION_MAXBUFFS_FORMAT = "%d |4buff:buffs";
 
-KRF_OPTION_FRIENDSCLASSCOLOR_NAMEPLATES = "Players nameplates colored by class (outside raid)";
+KRF_OPTION_FRIENDSCLASSCOLOR_NAMEPLATES = "Players nameplates colored by class (outside instances)";
 
 KRF_OPTION_RESET_OPTIONS = "Reset options";
-KRF_OPTION_RELOAD_REQUIRED = "Some options need a reload (write: /reload )";
+KRF_OPTION_RELOAD_REQUIRED = "Some changes require reloading (write: /reload )";
 
 KRF_OPTION_SHOWMSGNORMAL = GYL.."Display messages";
 KRF_OPTION_SHOWMSGWARNING = GYL.."Display warnings";
--- localization.fr.lua
+++ localization.fr.lua
@@ -8,13 +8,14 @@ KRF_VERS_TITLE    = format("%s %s", KRF_TITLE, KRF_VERSION);
 
 -- Whats new info
 KRF_WHATSNEW = OR.."- "..KRF_VERS_TITLE..YLL.." - Nouveaut\195\169s :|r\n"
-    .."- Mis \195\160 jour pour Shadowlands 9.2.7\n"
+    .."- Pr\195\170t pour DragonFlight (prepatch) 10.0.0\n"
+    .."- Retir\195\169s : Max buffs, et Toujours afficher les raids\n"
     ;
 
 KRF_SUBTITLE      = "Assistance frames de raid";
 KRF_DESC          = "Am\195\169liore les frames de groupe/raid et des unit\195\169s amies.\n\n"
-.." - Met en \195\169vidence le fond des joueurs en manque de soin\n\n"
-.." - Les barres invers\195\169es utiliseront directement vos couleurs\n\n"
+.." - Met en \195\169vidence le fond des joueurs en manque de vie\n\n"
+.." - Les barres invers\195\169es utiliseront votre choix de couleurs\n\n"
 .." - Transparence des unit\195\169s hors de port\195\169e";
 KRF_OPTIONS_TITLE = format("%s - Options", KRF_VERS_TITLE);
 
@@ -26,17 +27,17 @@ KRF_INIT_FAILED = format("%s pas charg\195\169 correctement !", KRF_VERS_TITLE);
 
 
 KRF_OPTION_RAID_HEADER = "Groupe / Raid";
-KRF_OPTION_HIGHLIGHTLOWHP = "Mettre en \195\169vidence la perte de PV des joueurs";
-KRF_OPTION_REVERTBAR = "Barres de vies "..YL.."invers\195\169es|r (moins on a de vie, plus la barre grandit !) "..YL.."*";
+KRF_OPTION_HIGHLIGHTLOWHP = "Mettre en \195\169vidence le manque de vie (couleurs dynamiques)"..YL.."*";
+KRF_OPTION_REVERTBAR = "Barres de "..YL.."vies invers\195\169es|r (moins on a de vie, plus la barre grandit !) ";
 KRF_OPTION_HEALTH_LOW = "Presque mort !";
 KRF_OPTION_HEALTH_LOW_TOOLTIP = "La couleur sera appliqu\195\169e "..YLL.."SOUS|r cette limite\n\n"
-    .."ex : Rouge sous 30%";
+    .."ex : Rouge sous 25%";
 KRF_OPTION_HEALTH_WARN = "Attention";
 KRF_OPTION_HEALTH_WARN_TOOLTIP = "La couleur sera appliqu\195\169e "..YLL.."\195\128|r cette limite\n\n"
     .."ex : Jaune \195\160 50%";
-KRF_OPTION_HEALTH_OK = "Vie ok";
+KRF_OPTION_HEALTH_OK = "Bonne sant\195\169";
 KRF_OPTION_HEALTH_OK_TOOLTIP = "La couleur sera appliqu\195\169e "..YLL.."AU-DESSUS|r DE cette limite\n\n"
-    .."ex : Vert apr\195\170s 60%";
+    .."ex : Vert apr\195\170s 75%";
 KRF_OPTION_MOVEROLEICONS = "Ic\195\180ne de r\195\180le en haut \195\160 gauche";
 KRF_OPTION_HIDEDAMAGEICONS = "Masquer l\'ic\195\180ne de r\195\180le 'd\195\169g\195\160ts'";
 KRF_OPTION_HIDEREALM = "Masquer le royaume des joueurs";
@@ -45,21 +46,22 @@ KRF_OPTION_NOTINRANGE = "Transparence si hors de port\195\169e";
 KRF_OPTION_NOTINCOMBAT = "Transparence du raid hors de combat";
 KRF_OPTION_SOLORAID = CY.."Toujours afficher les frames de raid "..YL.."*";
 KRF_OPTION_DEBUG_ON = "! Tester les frames !";
-KRF_OPTION_DEBUG_ON_MESSAGE = "Test des frames de raid activ\195\168, recliquez pour stopper !";
+KRF_OPTION_DEBUG_ON_MESSAGE = "Test des frames de raid activ\195\169 (testable en Mode \195\137dition)\n"
+                .."Recliquez pour stopper !";
 KRF_OPTION_DEBUG_OFF = "! ARR\195\138TER LE TEST !";
-KRF_OPTION_DEBUG_OFF_MESSAGE = "Test arr\195\170t\195\168, vous pouvez reprendre une activit\195\168 normale";
+KRF_OPTION_DEBUG_OFF_MESSAGE = "Test arr\195\170t\195\169, vous pouvez reprendre une activit\195\169 normale";
 
 KRF_OPTION_BUFFS_HEADER = "Buffs / Debuffs";
-KRF_OPTION_BUFFSSCALE = "Taille des buffs";
-KRF_OPTION_DEBUFFSSCALE = "Taille des d\195\169buffs";
+KRF_OPTION_BUFFSSCALE = "Taille des buffs *";
+KRF_OPTION_DEBUFFSSCALE = "Taille des d\195\169buffs *";
 KRF_OPTION_MAXBUFFS = "Afficher maximum";
 KRF_OPTION_MAXBUFFS_TOOLTIP = "Nombre maximum de buffs \195\160 afficher";
 KRF_OPTION_MAXBUFFS_FORMAT = "%d |4buff:buffs";
 
-KRF_OPTION_FRIENDSCLASSCOLOR_NAMEPLATES = "Barre d'info des unit\195\169s color\195\169e par classe (hors raid)";
+KRF_OPTION_FRIENDSCLASSCOLOR_NAMEPLATES = "Barre d'info des unit\195\169s color\195\169e par classe (hors instances)";
 
 KRF_OPTION_RESET_OPTIONS = "R\195\169initialiser le profil";
-KRF_OPTION_RELOAD_REQUIRED = "Certaines options requi\195\168rent un rechargement (\195\169crivez : /reload )";
+KRF_OPTION_RELOAD_REQUIRED = "Certains changements requi\195\168rent un rechargement (\195\169crivez : /reload )";
 
 KRF_OPTION_SHOWMSGNORMAL = GYL.."Afficher les messages";
 KRF_OPTION_SHOWMSGWARNING = GYL.."Afficher les alertes";
